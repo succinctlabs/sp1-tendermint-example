c9a7ff2dfcba67a2648bcdf6ec2543257a52ae5d
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-04-16 14:23:33 -0700

    feat: works with ethers (#3)
    * works with ethers
    
    * ethers
    
    * broadcast logs
    
    * verifying not stable

--- operator/src/main.rs
+++ operator/src/main.rs
@@ -1,15 +1,22 @@
 use std::env;
 use std::str::FromStr;
+use std::sync::Arc;
 use std::time::Duration;
 
 use alloy::primitives::Address;
-use alloy::providers::network::EthereumSigner;
-use alloy::providers::ProviderBuilder;
-use alloy::signers::wallet::LocalWallet;
-use alloy::signers::Signer;
+use ethers::contract::abigen;
+use ethers::middleware::SignerMiddleware;
+use ethers::providers::Middleware;
+use ethers::signers::Signer;
+use ethers::types::TransactionReceipt;
+// use alloy::providers::network::EthereumSigner;
+// use alloy::providers::ProviderBuilder;
+// use alloy::signers::wallet::LocalWallet;
 use reqwest::Client;
-use reqwest::Url;
 use sp1_sdk::{utils, ProverClient, PublicValues, SP1Stdin};
+
+use ethers::providers::{Http, Provider};
+use ethers::signers::LocalWallet;
 
 use sha2::{Digest, Sha256};
 use subtle_encoding::hex;
@@ -23,20 +30,20 @@ use crate::util::fetch_block;
 use crate::util::fetch_latest_commit;
 use crate::util::fetch_light_block;
 
-use alloy::sol;
+// sol! {
+//     #[sol(rpc)]
+//     contract SP1Tendermint {
+//         #[derive(Debug)]
+//         bytes32 public latestHeader;
 
-sol! {
-    #[sol(rpc)]
-    contract SP1Tendermint {
-        #[derive(Debug)]
-        bytes32 public latestHeader;
+//         function update(
+//             bytes calldata _publicValues,
+//             bytes calldata _proof
+//         );
+//     }
+// }
 
-        function update(
-            bytes calldata _publicValues,
-            bytes calldata _proof
-        );
-    }
-}
+abigen!(SP1Tendermint, "../abi/SP1Tendermint.abi.json");
 
 const TENDERMINT_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 mod util;
@@ -107,16 +114,14 @@ async fn prove_next_block_height_update(
 
     let mut stdin = SP1Stdin::new();
 
+    // TODO: normally we could just write the LightBlock, but bincode doesn't work with LightBlock.
+    // let encoded: Vec<u8> = bincode::serialize(&light_block_1).unwrap();
+    // let decoded: LightBlock = bincode::deserialize(&encoded[..]).unwrap();
     let encoded_1 = serde_cbor::to_vec(&trusted_light_block).unwrap();
     let encoded_2 = serde_cbor::to_vec(&target_light_block).unwrap();
 
     stdin.write_vec(encoded_1);
     stdin.write_vec(encoded_2);
-
-    // TODO: normally we could just write the LightBlock, but bincode doesn't work with LightBlock.
-    // The following code will panic.
-    // let encoded: Vec<u8> = bincode::serialize(&light_block_1).unwrap();
-    // let decoded: LightBlock = bincode::deserialize(&encoded[..]).unwrap();
 
     // Read SP1_PRIVATE_KEY from environment variable.
     let sp1_private_key = env::var("SP1_PRIVATE_KEY").unwrap();
@@ -159,7 +164,7 @@ async fn main() {
     utils::setup_logger();
 
     // BLOCK_INTERVAL defines which block to update to next.
-    let block_interval: u64 = 1000;
+    let block_interval: u64 = 10;
 
     // Read private key from environment variable.
     let private_key = env::var("PRIVATE_KEY").unwrap();
@@ -168,23 +173,28 @@ async fn main() {
     let rpc_url = env::var("RPC_URL").unwrap();
 
     loop {
-        let signer = LocalWallet::from_str(&private_key).unwrap();
-        let provider = ProviderBuilder::new()
-            .signer(EthereumSigner::from(signer))
-            .on_http(Url::parse(&rpc_url).unwrap())
-            .expect("could not connect to client");
+        let provider =
+            Provider::<Http>::try_from(rpc_url.clone()).expect("could not connect to client");
+
+        let signer = LocalWallet::from_str(&private_key)
+            .unwrap()
+            .with_chain_id(provider.get_chainid().await.unwrap().as_u64());
+
+        let client = Arc::new(SignerMiddleware::new(provider.clone(), signer.clone()));
+
+        println!("client: {:?}", client.get_chainid().await);
 
         let address: Address = Address::from_str(&env::var("CONTRACT_ADDRESS").unwrap()).unwrap();
 
-        let contract = SP1Tendermint::new(address, provider);
+        let contract = SP1Tendermint::new(address.0 .0, client);
 
-        let latest_header_hash_call = contract.latestHeader();
+        let trusted_header_hash = contract.latest_header().await.unwrap();
 
-        let trusted_header_hash = latest_header_hash_call.call().await.unwrap();
+        // let trusted_header_hash = latest_header_hash_call.call().await.unwrap();
 
         println!("Trusted header hash: {:?}", trusted_header_hash);
 
-        let trusted_light_block = get_light_block_by_hash(&trusted_header_hash._0.0).await;
+        let trusted_light_block = get_light_block_by_hash(&trusted_header_hash).await;
         let trusted_block_height = trusted_light_block.signed_header.header.height.value();
         println!(
             "Trusted light block height: {}",
@@ -200,16 +210,25 @@ async fn main() {
 
         if next_block_height < latest_block_height {
             let (trusted_light_block, target_light_block) =
-                get_light_blocks(&trusted_header_hash._0.0, next_block_height).await;
+                get_light_blocks(&trusted_header_hash, next_block_height).await;
 
             // Discard the proof bytes for now and update the
             let (pv, proof) =
                 prove_next_block_height_update(&trusted_light_block, &target_light_block).await;
 
             // Relay the proof to the contract.
-            let update_call = contract.update(pv.into(), proof.into());
-            let _ = update_call.call().await.unwrap();
-            let _ = update_call.send().await.unwrap();
+            let tx: Option<TransactionReceipt> = contract
+                .update(pv.into(), proof.into())
+                .send()
+                .await
+                .unwrap()
+                .await
+                .unwrap();
+            if tx.is_some() {
+                println!("Transaction hash: {:?}", tx.unwrap().transaction_hash);
+            } else {
+                println!("Transaction failed");
+            }
 
             println!(
                 "successfully generated and verified proof for the program! relayed to contract"
